2c1d56eba74687b4d8d20c2ba47c20f53337cb57
samouly <samuel.bounan@ens-lyon.fr>
2022-02-14 15:31:34 +0100

    address instead of ip

--- src/yaml.rs
+++ src/yaml.rs
@@ -34,8 +34,8 @@ pub fn read_server_address(hash_net_config: &Hash, i: u32) -> (String, u16) {
 
     let server_i: String = "server".to_owned() + &i.to_string();
 
-    let ip_yaml: Yaml = read_yaml(hash_net_config, &server_i, "ip");
-    let ip: String = ip_yaml.into_string()
+    let address_yaml: Yaml = read_yaml(hash_net_config, &server_i, "address");
+    let address: String = address_yaml.into_string()
         .expect("In yaml file, one ip adress is not of string type");
 
     let port_client: u16 = read_yaml(hash_net_config, &server_i, "port_client")
@@ -43,15 +43,15 @@ pub fn read_server_address(hash_net_config: &Hash, i: u32) -> (String, u16) {
         .expect("In yaml file, one port adress is not of int type")
         as u16;
     
-    (ip, port_client)
+    (address, port_client)
 }
 
 pub fn read_network_parameters(hash_net_config: &Hash) -> u32 {
 
-    let ip_yaml: u32 = read_yaml(hash_net_config, "parameters", "nb_servers")
+    let nb_servers: u32 = read_yaml(hash_net_config, "parameters", "nb_servers")
     .as_i64()
     .expect("In yaml file, nb_servers is not of int type")
     as u32;
 
-    ip_yaml
+    nb_servers
 }
