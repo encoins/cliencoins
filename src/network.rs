5500b5bb5098b833e8e4ea882b6a600bcee9825e
avincigu <arthur.vinciguerra@ens-lyon.fr>
2022-02-28 20:26:58 +0100

    - Some bugs were solved - Some doc was added - Still needs more work to make the code cleaner

--- src/network.rs
+++ src/network.rs
@@ -7,6 +7,7 @@ use std::io::{Write,Read};
 use std::sync::mpsc;
 use std::sync::mpsc::{Receiver, Sender};
 use std::thread;
+use std::time::Duration;
 use crate::yaml::*;
 use crate::instructions::Instruction;
 use crate::response::Response;
@@ -45,7 +46,7 @@ pub fn connect_to_network(stream: &mut Option<TcpStream>)
     // Tries to connect to each server until it achieves to do so.
     for i in 1..nb_nodes+1
     {
-        let id_node = (i +rd_start)%nb_nodes;
+        let id_node = (i +rd_start)%nb_nodes + 1;
         let address = read_server_address(&hash_net_config, id_node);
         //println!("Trying to connect to node {}...", id_node);
 
@@ -100,16 +101,17 @@ pub fn send_instruction(stream : &mut Option<TcpStream>, instruction : Instructi
     }
 }
 
-
-
+/// Gets the balance of a given user by broadcasting a request to every servers
 pub fn get_balance(user : &UserId, main_sender : &Sender<String>)
 {
     let hash_net_config = yaml_to_hash("net_config.yml");
     let nb_nodes = read_network_parameters(&hash_net_config);
     let mut nb_answers = 0;
+    let mut nb_error_answ = 0;
     let mut answers : HashMap<Currency, u32> = HashMap::new();
     let us = user.clone();
     let local_ms = main_sender.clone();
+
     // Spawn a thread to deal with balance
     thread::spawn(
         move ||
@@ -137,42 +139,43 @@ pub fn get_balance(user : &UserId, main_sender : &Sender<String>)
                 // Now deal with the response
                 while nb_answers != nb_nodes
                 {
-                    match receiver.recv()
+                    // The threads above will send their result in the given receiver
+
+                    match receiver.recv().expect("Fatal error: this should never happen!")
                     {
-                        Ok(res) =>
+                        // This thread should always be able to receive.
+
+                        Ok(response) =>
                             {
-                                nb_answers+=1;
-                                match res
+                                match response
                                 {
-                                    Ok(res) =>
+                                    Response::Balance(cur) =>
                                         {
-                                            match res
+                                            // This code block increments by one the number of servers agreeing for amount "cur" in the hashmap answers
+                                            match answers.get(&cur)
                                             {
-                                                Response::Balance(cur) =>
+                                                None =>
                                                     {
-                                                        match answers.get(&cur)
-                                                        {
-                                                            None =>
-                                                                {
-                                                                    answers.insert(cur, 1);
-                                                                }
-                                                            Some(j) =>
-                                                                {
-                                                                    answers.insert(cur, j+1);
-                                                                }
-                                                        }
+                                                        answers.insert(cur, 1);
                                                     }
-                                                _ => {}
+                                                Some(j) =>
+                                                    {
+                                                        answers.insert(cur, j+1);
+                                                    }
                                             }
                                         }
-                                    Err(_) => {}
+                                    _ => {}
                                 }
                             }
-                        Err(err) =>
+
+                        Err(_) =>
                             {
-                                panic!("Fatal error while trying to make the balance request : {}", err.to_string());
+                                nb_error_answ +=1;
                             }
+
                     }
+
+                    nb_answers+=1;
                 }
 
                 // Transform response into a string to return
@@ -186,15 +189,28 @@ pub fn get_balance(user : &UserId, main_sender : &Sender<String>)
                         }
                     1 =>
                         {
-                            rtn_str = format!("All nodes in the network agree that the user has {} encoins", answ_it.next().unwrap().0);
+                            if nb_error_answ >0
+                            {
+                                rtn_str = format!("All nodes that answered agree that the user has {} encoins ({} servers did not answer in time)", answ_it.next().unwrap().0, nb_error_answ);
+                            }
+                            else
+                            {
+                                rtn_str = format!("All nodes in the network agree that the user has {} encoins", answ_it.next().unwrap().0);
+                            }
+
                         }
                     _ =>
                         {
                             rtn_str.push_str("Nodes don't all agree on the balance of the user : ");
+                            if nb_error_answ > 0
+                            {
+                                rtn_str = format!("{}({} servers did not answer in time)", rtn_str, nb_error_answ)
+                            }
                             for answ in answ_it
                             {
                                 rtn_str = format!("{}\n- {} according to {} servers", rtn_str, answ.0, answ.1);
                             }
+
                         }
                 }
 
@@ -206,6 +222,7 @@ pub fn get_balance(user : &UserId, main_sender : &Sender<String>)
     );
 }
 
+/// Makes a transfer request to a randomly chosen server
 pub fn make_transfer(instruction : Instruction, main_sender: &Sender<String>)
 {
     let ms_copy = main_sender.clone();
@@ -228,6 +245,24 @@ pub fn make_transfer(instruction : Instruction, main_sender: &Sender<String>)
     return;
 }
 
+/// Spawns a thread that deals with the communication of a given message
+///
+/// # Generic arguments
+///
+/// * `MessageT` - Type of the message to send to a server
+/// * `ResponseT` - Type of the expected server's response
+/// * `ResponseHandlingT` - Type of the enclosure used to handle the server's response
+///
+/// # Arguments
+///
+/// * `message` - Message to be sent to a server
+/// * `server_address` - Address of the server to send the message to. A random server will be chosen if it has `None` value
+/// * `response_handler` - Enclosure to be used to deal with the server's response
+/// * `thread_lifetime` - Maximum time (in milliseconds) that the thread will wait for the server's response before ending. Lifetime is set to 10 seconds if it has `None` value
+///
+/// # Examples
+///
+/// See [`make_transfer`]
 fn spawn_response_thread<
     MessageT:serde::Serialize + Send + 'static,
     ResponseT: DeserializeOwned + 'static,
@@ -237,58 +272,14 @@ fn spawn_response_thread<
     message : MessageT,
     server_address : Option<(String,u16)>,
     response_handler: ResponseHandlingT,
-    max_wait_time : Option<u64>
+    thread_lifetime: Option<u64>
 )
 {
     thread::spawn(
         move ||
             {
-                // First connect to a specific or a random server
-                let mut stream;
-                match server_address
-                {
-                    None =>
-                        {
-                            let mut part_stream : Option<TcpStream>= None;
-                            loop
-                            {
-                                match part_stream
-                                {
-                                    None =>
-                                        {
-                                            connect_to_network(&mut part_stream);
-                                        }
-                                    Some(tcp) =>
-                                        {
-                                            stream = tcp;
-                                            break;
-                                        }
-                                }
-                            }
-                        }
-                    Some(address) =>
-                        {
-                            loop
-                            {
-                                match TcpStream::connect(address.clone())
-                                {
-                                    Ok(tcpstr) =>
-                                        {
-                                            stream = tcpstr;
-                                            break;
-                                        }
-                                    Err(_) => {}
-                                }
-
-                            }
-                        }
-                };
-
-                // Then send the desired request
-                let msg = &(bincode::serialize(&message).unwrap()[..]);
-                stream.write(msg);
-
-                let wait_time  = match max_wait_time // in milliseconds
+                // First determine thread's lifetime
+                let mut remaining_lifetime = match thread_lifetime // in milliseconds
                 {
                     None =>
                         {
@@ -300,56 +291,135 @@ fn spawn_response_thread<
                         }
                 };
 
-                // Loop to try receiving the message for a given wait time
-                let mut nb_wait = 0;
-                let mut buf;
-                let mut received = false;
-                loop
+
+                // Then try to connect to a specific or a random server.
+                let mut opt_stream = None;
+                let mut time_looped = Duration::new(0, 0);
+                match server_address
                 {
-                    buf = [0;100];
-                    match stream.read(&mut buf)
-                    {
-                        Ok(rcv_size) =>
+                    None =>
+                        {
+                            let mut part_stream : Option<TcpStream>= None;
+                            let loop_start = std::time::Instant::now();
+                            // Loop while the client does not manage to connect to a server
+                            loop
                             {
-                                if rcv_size < 1 {}
-                                else
+                                match part_stream
                                 {
-                                    // Stop looping
-                                    received = true;
+                                    None =>
+                                        {
+                                            connect_to_network(&mut part_stream);
+                                        }
+                                    Some(tcp) =>
+                                        {
+                                            opt_stream = Some(tcp);
+                                            remaining_lifetime -= time_looped;
+                                            break;
+                                        }
+                                }
+
+                                time_looped = std::time::Instant::now() - loop_start;
+
+                                if time_looped > remaining_lifetime
+                                {
+                                    response_handler(Err(String::from("Could not connect to a server! (Are you connected to the internet?)")));
                                     break;
                                 }
                             }
-                        Err(_) => { } // Continue looping
-                    }
-                    nb_wait+=1;
-                    thread::sleep(time::Duration::from_millis(50));
-
-                    if nb_wait*50 > wait_time.as_millis()
-                    {
-                        break;
-                    }
-
-                }
-
-                // Send a response if there is one
-                if !received
-                {
-                   response_handler(Err(format!("No response from server")));
-                }
-                else
-                {
-                    let response : bincode::Result<ResponseT> = deserialize(&buf[..]);
-                    match response
-                    {
-                        Ok(rcv) =>
+                        }
+                    Some(address) =>
+                        {
+                            let loop_start = std::time::Instant::now();
+                            loop
                             {
-                                response_handler(Ok(rcv));
+                                match TcpStream::connect(address.clone())
+                                {
+                                    Ok(tcpstr) =>
+                                        {
+                                            opt_stream = Some(tcpstr);
+                                            remaining_lifetime -= time_looped;
+                                            break;
+                                        }
+                                    Err(_) => {}
+                                }
+
+                                time_looped = std::time::Instant::now() - loop_start;
+
+                                if time_looped > remaining_lifetime
+                                {
+                                    response_handler(Err(format!("Could not connect to the server {}! (The server is byzantine or you are not connected to the internet)", address.0)));
+                                    break;
+                                }
+
                             }
-                        Err(_) =>
+                        }
+                };
+
+
+                match opt_stream
+                {
+                    None => {}
+                    Some(mut stream) =>
+                        {
+                            // Then send the desired request
+                            let msg = &(bincode::serialize(&message).unwrap()[..]);
+                            stream.write(msg);
+
+
+                            // Loop to try receiving the message for a given wait time
+                            time_looped = Duration::new(0,0);
+                            let loop_start = std::time::Instant::now();
+                            let mut buf;
+                            let mut received = false;
+                            loop
                             {
-                                response_handler(Err(format!("Error when trying to read response from server")));
+                                buf = [0;100];
+                                match stream.read(&mut buf)
+                                {
+                                    Ok(rcv_size) =>
+                                        {
+                                            if rcv_size < 1 {}
+                                            else
+                                            {
+                                                // Stop looping
+                                                received = true;
+                                                break;
+                                            }
+                                        }
+                                    Err(_) => { } // Continue looping
+                                }
+                                thread::sleep(time::Duration::from_millis(50));
+
+                                time_looped = std::time::Instant::now() - loop_start;
+
+                                if time_looped > remaining_lifetime
+                                {
+                                    break;
+                                }
+
                             }
-                    }
+
+                            // Send a response if there is one
+                            if !received
+                            {
+                                response_handler(Err(format!("No response from server")));
+                            }
+                            else
+                            {
+                                let response : bincode::Result<ResponseT> = deserialize(&buf[..]);
+                                match response
+                                {
+                                    Ok(rcv) =>
+                                        {
+                                            response_handler(Ok(rcv));
+                                        }
+                                    Err(_) =>
+                                        {
+                                            response_handler(Err(format!("Error when trying to read response from server")));
+                                        }
+                                }
+                            }
+                        }
                 }
             }
     );
